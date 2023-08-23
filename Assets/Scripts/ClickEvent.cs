b6d667746767792430fe2200ba104c0cf0a841a8
Amir Bekmetov <lebido_on@mail.ru>
2023-08-23 11:40:59 +0500

    Animation/Button

--- Assets/Scripts/ClickEvent.cs
+++ Assets/Scripts/ClickEvent.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ClickEvent : MonoBehaviour
 {
     [SerializeField] int money;
     public TextMeshProUGUI moneyText;
 
+    private void Start()
+    {
+        money = PlayerPrefs.GetInt("money");
+    }
     public void clickEvent()
     {
         money++;
+        PlayerPrefs.SetInt("money", money);
+    }
+
+    public void ToAchievements()
+    {
+        SceneManager.LoadScene(1);
     }
 
     void Update()
